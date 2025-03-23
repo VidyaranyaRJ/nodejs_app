e67e4026666e8b488c67bd3602b2ef048fd01f39
Vidyaranya Javalagi <vrjavalagi779@gmail.com>
2025-03-23 10:01:46 -0600

    apply4

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -51,8 +51,8 @@ resource "aws_security_group" "ecs_security_group" {
   }
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 3000
+    to_port     = 3000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
