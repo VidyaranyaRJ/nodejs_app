507f942c73e0a90563c9ec53f11ac8b881356e5c
Vidyaranya Javalagi <vrjavalagi779@gmail.com>
2025-03-23 12:35:32 -0600

    apply25

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -30,32 +30,6 @@ resource "aws_instance" "ecs_instance" {
   }
 }
 
-# resource "aws_ecs_task_definition" "my_node_app_task" {
-#   family                   = "my-node-app-task"
-#   execution_role_arn       = var.ecs_execution_role_arn  # Reference the output from IAM module
-#   task_role_arn            = var.ecs_task_role_arn  # Reference the output from IAM module
-#   network_mode             = "awsvpc"
-#   requires_compatibilities = ["FARGATE"]
-#   cpu                      = "256"
-#   memory                   = "512"
-  
-#   container_definitions = jsonencode([
-#     {
-#       name      = "my-node-app-container"
-#       image     = "${aws_ecr_repository.test.repository_url}:latest"
-#       cpu       = 256
-#       memory    = 512
-#       essential = true
-#       portMappings = [
-#         {
-#           containerPort = 80
-#           hostPort      = 80
-#         }
-#       ]
-#     }
-#   ])
-#   depends_on = [ aws_ecr_repository.test ]
-# }
 resource "aws_ecs_task_definition" "my_node_app_task" {
   family                   = "my-node-app-task"
   execution_role_arn       = var.ecs_execution_role_arn
