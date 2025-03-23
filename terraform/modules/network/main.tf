fed554b885d13dc78b9cf567483d5e3084c4b2d2
Vidyaranya Javalagi <vrjavalagi779@gmail.com>
2025-03-23 11:02:23 -0600

    apply9

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -38,10 +38,15 @@ resource "aws_route_table" "my_route_table" {
   depends_on = [  aws_vpc.my_vpc, aws_internet_gateway.my_gateway ]
 }
 
-resource "aws_route" "public_route" {
-  route_table_id         = aws_route_table.my_route_table.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.my_gateway.id
+# resource "aws_route" "public_route" {
+#   route_table_id         = aws_route_table.my_route_table.id
+#   destination_cidr_block = "0.0.0.0/0"
+#   gateway_id             = aws_internet_gateway.my_gateway.id
+# }
+
+resource "aws_route_table_association" "public_association" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_route_table.id
 }
 
 
