216e20a4709f38e497fcf8279074c21659fa2609
Vidyaranya Javalagi <vrjavalagi779@gmail.com>
2025-03-22 18:58:01 -0600

    ca11

---
+++ terraform/modules/IAM/main.tf
@@ -0,0 +1,53 @@
+resource "aws_iam_role" "ecs_execution_role" {
+  name = var.ecs_execution_name
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonECS_FullAccess"
+  role       = aws_iam_role.ecs_execution_role.name
+}
+
+
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = var.ecs_task_name
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+      Effect    = "Allow"
+      Sid       = ""
+    }]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "ecs_task_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"  # Example policy for S3
+  role       = aws_iam_role.ecs_task_role.name
+}
---
+++ terraform/modules/network/main.tf
@@ -0,0 +1,62 @@
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "my-node-js-vpc"
+  }
+}
+
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-2a"  # Change to your region’s AZ
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "my-subnet-node-js"
+  }
+  depends_on = [  aws_vpc.my_vpc ]
+}
+
+resource "aws_internet_gateway" "my_gateway" {
+  vpc_id = aws_vpc.my_vpc.id
+  depends_on = [  aws_vpc.my_vpc ]
+}
+
+
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_gateway.id
+  }
+  depends_on = [  aws_vpc.my_vpc, aws_internet_gateway.my_gateway ]
+}
+
+
+
+
+resource "aws_security_group" "ecs_security_group" {
+  name        = var.sg_name
+  description = "Allow inbound traffic to ECS tasks"
+  vpc_id      = aws_vpc.my_vpc.id  
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  depends_on = [  aws_vpc.my_vpc ]
+}
+
+
